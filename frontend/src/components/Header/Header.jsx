5748583be0fe60ba123719c6876670b9d678b63e
FIRST_NAME LAST_NAMEUdtrx <dutrieux.ulysse@gmail.com>
2023-12-18 20:20:33 +0100

    Create first Template of an Header's responsive

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import "./header.css";
 
 function Header() {
-  return <> </>;
+  return (
+    <nav className="Navbar">
+      <h1 className="Title-Header">Street Art Hunterz</h1>
+      <div className="LogOut-img">
+        <img
+          id="LogOut-Header"
+          src="./assets/images/ico/login_FILL0_wght400_GRAD0_opsz24.png"
+          alt="log-out"
+        />
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
