d15ece6de22b15a1eea38dc133822a6f43af5a50
FIRST_NAME LAST_NAMEUdtrx <dutrieux.ulysse@gmail.com>
2023-12-19 10:14:22 +0100

    Add logo to header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,11 +5,15 @@ function Header() {
     <nav className="Navbar">
       <h1 className="Title-Header">Street Art Hunterz</h1>
       <div className="LogOut-img">
-        <img
-          id="LogOut-Header"
-          src="./assets/images/ico/login_FILL0_wght400_GRAD0_opsz24.png"
-          alt="log-out"
-        />
+        <svg
+          className="LogOut-Header"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
+        </svg>
       </div>
     </nav>
   );
