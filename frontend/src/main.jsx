33ae32e2fac8be86c27d6c9ea286e8f89142c429
FIRST_NAME LAST_NAMEUdtrx <dutrieux.ulysse@gmail.com>
2023-12-20 14:06:30 +0100

    test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import App from "./App";
 import IntroPage from "./pages/IntroPage/IntroPage";
 import HomePage from "./pages/HomePage/HomePage";
