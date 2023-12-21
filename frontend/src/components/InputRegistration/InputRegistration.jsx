7829bf27b79a4f2a7d2c700bba7068e31c9bf97b
FIRST_NAME LAST_NAMEUdtrx <dutrieux.ulysse@gmail.com>
2023-12-21 15:57:04 +0100

    Create register page

---
+++ frontend/src/components/InputRegistration/InputRegistration.jsx
@@ -0,0 +1,64 @@
+import { useNavigate } from "react-router-dom";
+import anonymous from "../../assets/images/img/pngwing.com.png";
+import paintMan from "../../assets/images/img/paint-man.png";
+import "./inputRegistration.css";
+
+function InputRegistration() {
+  const navigate = useNavigate();
+  return (
+    <section className="Display_Desktop_Register">
+      <div className="Picture_DesKtop_Register">
+        <img
+          className="PaintMan_Picture_Register_left"
+          src={paintMan}
+          alt="paintMan"
+        />
+      </div>
+      <div className="Block_Register">
+        <div className="Register">
+          <h1 className="Title_Register">REGISTER</h1>
+          <div className="Pseudo_Register">
+            <h3>Choose a pseudo</h3>
+            <input className="Input_Register" type="text" placeholder="" />
+          </div>
+          <div className="Password_Register">
+            <h3>Choose a password</h3>
+            <input className="Input_Register" type="password" placeholder="" />
+          </div>
+          <div className="Pseudo_Register">
+            <h3>Enter your mail</h3>
+            <input className="Input_Register" type="text" placeholder="" />
+          </div>
+          <div
+            className="Button-Register"
+            role="button"
+            onClick={() => {
+              navigate("/");
+            }}
+            onKeyDown={() => {
+              navigate("/");
+            }}
+            tabIndex="0"
+          >
+            VALIDATION
+          </div>
+        </div>
+        <div className="Anonymous_Register">
+          <img
+            className="Anonymus_Picture_Register"
+            src={anonymous}
+            alt="Anonymous"
+          />
+        </div>
+      </div>
+      <div className="Picture_DesKtop_Register">
+        <img
+          className="PaintMan_Picture_Register"
+          src={paintMan}
+          alt="paintMan"
+        />
+      </div>
+    </section>
+  );
+}
+export default InputRegistration;
