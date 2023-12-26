b234c2153b33377791616a70053ed446c0862563
JULIEN BONET <julienbonetpro@gmail.com>
2023-12-26 19:17:33 +0100

    created admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import FooterNotConnected from "./components/Footer/FooterNotConnected";
-
 import "./App.css";
+import AdminProfil from "./pages/AdminProfil/AdminProfil";
 
 function App() {
   return (
     <main>
       <Header />
-      <Outlet />
+      <AdminProfil />
       <FooterNotConnected />
     </main>
   );
--- frontend/src/pages/AdminProfil/AdminProfil.jsx
+++ frontend/src/pages/AdminProfil/AdminProfil.jsx
@@ -1,4 +1,34 @@
+import "./adminProfil.css";
+import WorkCard from "../../components/WorkCard/WorkCard";
+
 function AdminProfil() {
-  return <> </>;
+  return (
+    <section className="AdminProfilContainer">
+      <h1 className="Pseudo_admin">PSEUDO ADMIN</h1>
+      <div className="Seniority_admin_container">
+        <p className="Seniority_admin"> ADMIN FROM 22/11/2022 </p>
+      </div>
+      <div className="ProfilInfos_container_admin">
+        <div className="admin_title_box">
+          <h2 className="ProfilInfos_title_admin">PROFIL INFOS</h2>
+        </div>
+        <div className="Profil_infos_content_admin">
+          <p className="admin_infos">EMAIL: exemple@gmail.com</p>
+          <p className="admin_infos">PASSWORD: ******</p>
+          <div className="admin_infos_btn">
+            <p className="text_admin_infos_btn">change password</p>
+          </div>
+        </div>
+      </div>
+      <div className="history_container_admin">
+        <div className="admin_history_title_box">
+          <h2 className="history_title_admin">HISTORICAL</h2>
+        </div>
+        <div className="historyWorkSubmit_admin"> WORK SUBMIT : 34 </div>
+        <WorkCard />
+      </div>
+      <section className="admin_features"> </section>
+    </section>
+  );
 }
 export default AdminProfil;
