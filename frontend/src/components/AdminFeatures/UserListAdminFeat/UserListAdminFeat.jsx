f262c91ccfca0e843fbebc22d1c2194aff8aef6a
JULIEN BONET <julienbonetpro@gmail.com>
2023-12-28 15:15:27 +0100

    completed user list part as far as possible + created toggle historical and admin info

--- frontend/src/components/AdminFeatures/UserListAdminFeat/UserListAdminFeat.jsx
+++ frontend/src/components/AdminFeatures/UserListAdminFeat/UserListAdminFeat.jsx
@@ -1,3 +1,9 @@
+import { useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+
 import "./userListAdminFeat.css";
 import DataUsers from "../../../../data_sample/data_users.json";
 
@@ -5,6 +11,22 @@ function UserListAdminFeat() {
   const data = DataUsers;
   const UsersCount = data.length;
 
+  // state pagination table //
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
+  // state pagination modal //
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <section className="ULAF_container">
       <div className="searchBar_ULAF_container">
@@ -14,7 +36,10 @@ function UserListAdminFeat() {
           className="searchBar_ULAF"
         />
       </div>
-      <div className="hunters_count_ULAF"> hunters : {UsersCount}</div>
+      <div className="hunters_count_ULAF">
+        {" "}
+        hunters : <span className="font_info_color">{UsersCount}</span>
+      </div>
       <section className="ULAF_content">
         <div className="ULAF_toogle_Line">
           <div className="ULAF_toogle" id="ULAF_by_pseudo">
@@ -28,17 +53,41 @@ function UserListAdminFeat() {
           </div>
         </div>
         <section className="Users_List_ULAF">
-          <table>
-            {data.map((users) => (
-              <tr key={users.id}>
-                <td>{users.pseudo}</td>
-                <td>{users.score}</td>
-                <td>{users.registration}</td>
+          <table className="UsersList_Table_ULAF">
+            {currentItems.map((users, index) => (
+              <tr
+                onClick={handleOpen}
+                className="UsersList_Tr_ULAF"
+                key={users.id}
+              >
+                <td className="UsersList_Td_ULAF">
+                  {(currentPage - 1) * itemsPerPage + index + 1}
+                </td>
+                <td className="UsersList_Td_ULAF">{users.pseudo}</td>
+                <td className="UsersList_Td_ULAF">{users.score}</td>
+                <td className="UsersList_Td_ULAF">{users.registration}</td>
               </tr>
             ))}
           </table>
+          {/* Pagination table */}
+          <Stack spacing={2} mt={2}>
+            <Pagination
+              count={Math.ceil(data.length / itemsPerPage)}
+              size="small"
+              shape="rounded"
+              variant="outlined"
+              siblingCount={0}
+              page={currentPage}
+              onChange={handlePageChange}
+            />
+          </Stack>
         </section>
       </section>
+      <Modal open={open} onClose={handleClose}>
+        <Box>
+          <div> THIS IS MODAL IN PROGRESS</div>
+        </Box>
+      </Modal>
     </section>
   );
 }
--- frontend/src/pages/AdminProfil/AdminProfil.jsx
+++ frontend/src/pages/AdminProfil/AdminProfil.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import "./adminProfil.css";
 import WorkCard from "../../components/WorkCard/WorkCard";
 import UserListAdminFeat from "../../components/AdminFeatures/UserListAdminFeat/UserListAdminFeat";
 
 function AdminProfil() {
+  // Toggle Admin Profil Info //
+  const [adminInfoIsOpen, setAdminInfoIsOpen] = useState(false);
+
+  const toggleAdminInfo = () => {
+    setAdminInfoIsOpen(!adminInfoIsOpen);
+  };
+  // Toggle historical Info //
+  const [adminHistoricalIsOpen, setAdminHistoricalIsOpen] = useState(false);
+
+  const toggleAdminHistorical = () => {
+    setAdminHistoricalIsOpen(!adminHistoricalIsOpen);
+  };
+
   return (
     <section className="AdminProfilContainer">
       <h1 className="Pseudo_admin">PSEUDO ADMIN</h1>
@@ -12,46 +26,103 @@ function AdminProfil() {
       <div className="ProfilInfos_container_admin">
         <div className="admin_title_box">
           <h2 className="ProfilInfos_title_admin">profil infos</h2>
+          {!adminInfoIsOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="35"
+              viewBox="0 -960 960 960"
+              width="35"
+              className="arrow_toggle_admin"
+              id="btn_Profil_Info_admin_open"
+              onClick={toggleAdminInfo}
+            >
+              <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="35"
+              viewBox="0 -960 960 960"
+              width="35"
+              className="arrow_toggle_admin"
+              id="btn_Profil_Info_admin_closed"
+              onClick={toggleAdminInfo}
+            >
+              <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+            </svg>
+          )}
         </div>
-        <div className="Profil_infos_content_admin">
-          <p className="admin_infos">email: exemple@gmail.com</p>
-          <p className="admin_infos">password: ******</p>
-          <div className="admin_infos_btn">
-            <p className="text_admin_infos_btn">change password</p>
+        {adminInfoIsOpen && (
+          <div className="Profil_infos_content_admin">
+            <p className="admin_infos">email: exemple@gmail.com</p>
+            <p className="admin_infos">password: ******</p>
+            <div className="admin_infos_btn">
+              <p className="text_admin_infos_btn">change password</p>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <div className="history_container_admin">
         <div className="admin_history_title_box">
           <h2 className="history_title_admin">historical</h2>
+          {!adminHistoricalIsOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="35"
+              viewBox="0 -960 960 960"
+              width="35"
+              className="arrow_toggle_admin"
+              id="btn_Profil_Info_admin_open"
+              onClick={toggleAdminHistorical}
+            >
+              <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="35"
+              viewBox="0 -960 960 960"
+              width="35"
+              className="arrow_toggle_admin"
+              id="btn_Profil_Info_admin_closed"
+              onClick={toggleAdminHistorical}
+            >
+              <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+            </svg>
+          )}
         </div>
-        <div className="historyWorkSubmit_admin"> works submitted : 34 </div>
-        <div className="admin_workcard_container">
-          <WorkCard />
-        </div>
-        <hr className="dashed_line_admin" />
-        <div className="admin_trash_btn">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="35"
-            viewBox="0 -960 960 960"
-            width="35"
-          >
-            <path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" />
-          </svg>
-        </div>
+        {adminHistoricalIsOpen && (
+          <>
+            <div className="historyWorkSubmit_admin">
+              {" "}
+              works submitted : 34{" "}
+            </div>
+            <div className="admin_workcard_container">
+              <WorkCard />
+            </div>
+            <hr className="dashed_line_admin" />
+            <div className="admin_trash_btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="35"
+                viewBox="0 -960 960 960"
+                width="35"
+              >
+                <path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" />
+              </svg>
+            </div>
+          </>
+        )}
       </div>
       <section className="admin_features">
         <div className="Line_BtnToggle_adminFeat">
           <div className="BtnToggle_adminFeat" id="usersList_admin">
-            {" "}
             users list
           </div>
           <div className="BtnToggle_adminFeat" id="worksList_admin">
             works list
           </div>
           <div className="BtnToggle_adminFeat" id="workValidation_admin">
-            {" "}
             validation
           </div>
         </div>
