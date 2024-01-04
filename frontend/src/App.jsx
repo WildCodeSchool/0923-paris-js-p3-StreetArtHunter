8f31e632218bc009179017865e64904097ef2d9c
JULIEN BONET <julienbonetpro@gmail.com>
2024-01-04 17:19:50 +0100

    fixed outlet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import FooterNotConnected from "./components/Footer/FooterNotConnected";
 import "./App.css";
-import AdminProfil from "./pages/AdminProfil/AdminProfil";
 
 function App() {
   return (
     <main>
       <Header />
-      <AdminProfil />
+      <Outlet />
       <FooterNotConnected />
     </main>
   );
