2ca6edeb6c74a95ae1447e6fe889d42235418312
JULIEN BONET <julienbonetpro@gmail.com>
2024-01-06 19:37:19 +0100

    fix admin profil height

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import FooterNotConnected from "./components/Footer/FooterNotConnected";
 import "./App.css";
+// import AdminProfil from "./pages/AdminProfil/AdminProfil";
 
 function App() {
   return (
--- frontend/src/components/AdminFeatures/ValidationAdminFeat/ValidationAdminFeat.jsx
+++ frontend/src/components/AdminFeatures/ValidationAdminFeat/ValidationAdminFeat.jsx
@@ -88,57 +88,60 @@ function ValidationAdminFeat() {
       )}
       {desktopScreen && (
         <section className="VLAF_workcards_Desktop">
-          <div className="VLAF_WC_DeskT_01">
-            <div className="VLAF_workcard_container">
-              {currentItems.map((data) => (
-                <WorkCard
-                  key={data.id}
-                  data={data}
-                  id="workCard_IAP"
-                  classForWCVADF={isWide}
-                />
-              ))}
+          <div className="VLAF_workcards_container">
+            <div className="VLAF_WC_DeskT_01">
+              <div className="VLAF_workcard_container">
+                {currentItems.map((data) => (
+                  <WorkCard
+                    key={data.id}
+                    data={data}
+                    id="workCard_IAP"
+                    classForWCVADF={isWide}
+                  />
+                ))}
+              </div>
             </div>
-            {/* Pagination */}
-            {data.length > itemsPerPage && (
-              <Stack spacing={0} mt={0}>
-                <Pagination
-                  count={Math.ceil(newSubmit.length / itemsPerPage)}
-                  size="small"
-                  shape="rounded"
-                  variant="outlined"
-                  siblingCount={0}
-                  page={currentPage}
-                  onChange={handlePageChange}
-                />
-              </Stack>
-            )}
-          </div>
 
-          <div className="VLAF_WC_DeskT_02">
-            <div className="VLAF_btn_container">
-              <div className="VLAF_valid_btn">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="35"
-                  viewBox="0 -960 960 960"
-                  width="35"
-                >
-                  <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
-                </svg>
-              </div>
-              <div className="VLAF_trash_btn">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="35"
-                  viewBox="0 -960 960 960"
-                  width="35"
-                >
-                  <path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" />
-                </svg>
+            <div className="VLAF_WC_DeskT_02">
+              <div className="VLAF_btn_container">
+                <div className="VLAF_valid_btn">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="35"
+                    viewBox="0 -960 960 960"
+                    width="35"
+                  >
+                    <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+                  </svg>
+                </div>
+                <div className="VLAF_trash_btn">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="35"
+                    viewBox="0 -960 960 960"
+                    width="35"
+                  >
+                    <path d="m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z" />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
+          <hr className="VLAF_solid_line" />
+          {/* Pagination */}
+          {data.length > itemsPerPage && (
+            <Stack spacing={0} mt={0}>
+              <Pagination
+                count={Math.ceil(newSubmit.length / itemsPerPage)}
+                size="small"
+                shape="rounded"
+                variant="outlined"
+                siblingCount={0}
+                page={currentPage}
+                onChange={handlePageChange}
+              />
+            </Stack>
+          )}
         </section>
       )}
     </section>
