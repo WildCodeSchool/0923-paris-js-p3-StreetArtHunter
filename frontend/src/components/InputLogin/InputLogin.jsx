74018c71c2fb85dcd364e5f7993a6965031d2bf9
JULIEN BONET <julienbonetpro@gmail.com>
2024-01-07 14:13:52 +0100

    fixed generalHeight smartph Admin Profil Page

--- frontend/src/components/InputLogin/InputLogin.jsx
+++ frontend/src/components/InputLogin/InputLogin.jsx
@@ -6,7 +6,7 @@ import "./inputLogin.css";
 function InputRegistration() {
   const navigate = useNavigate();
   return (
-    <section className="Display_Desktop_Login Global_height">
+    <section className="login_Container Display_Desktop_Login Global_height_smartPh Global_height">
       <div className="Picture_DesKtop_Login">
         <img
           className="loginonymous_left"
