040a0974a2793aa5b926024cdc62afa37e46178b
JULIEN BONET <julienbonetpro@gmail.com>
2024-01-12 16:00:10 +0100

    code desktop

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import "./homePage.css";
@@ -5,21 +6,34 @@ import "./homePage.css";
 import MapSample2 from "../../assets/images/map_sample/map_sample_home.jpg";
 
 function HomePage() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#141414",
+      },
+    },
+  });
   return (
-    <section className="homepage_Container">
+    <section className="homepage_Container Global_height">
       <p className="intro_text_homePage">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut hendrerit
         metus nec finibus faucibus. Phasellus in nunc ex. In efficitur massa
         tortor, sed mollis justo efficitur vitae.{" "}
       </p>
-      <img src={MapSample2} alt="map" className="map_homePage" />
-      <div className="cardinal_button_line">
-        <Stack spacing={2} direction="row">
-          <Button variant="outlined">NORTH</Button>
-          <Button variant="outlined">SOUTH</Button>
-          <Button variant="outlined">EAST</Button>
-          <Button variant="outlined">WEST</Button>
-        </Stack>
+      <div className="map_homePage_container">
+        <img src={MapSample2} alt="map" className="map_homePage" />
+      </div>
+      <div className="cardinal_buttons_line">
+        <ThemeProvider theme={theme}>
+          <Stack spacing={2} direction="row">
+            <Button variant="outlined" color="primary">
+              NORTH
+            </Button>
+            <Button variant="outlined">SOUTH</Button>
+            <Button variant="outlined">EAST</Button>
+            <Button variant="outlined">WEST</Button>
+          </Stack>
+        </ThemeProvider>
       </div>
     </section>
   );
