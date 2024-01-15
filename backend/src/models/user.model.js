81904ed1528b78b8a29a55c038ee58125c98ba37
FIRST_NAME LAST_NAMEUdtrx <dutrieux.ulysse@gmail.com>
2024-01-15 14:32:45 +0100

    Revert "Finish SpotZone & SpotZoneById"
    This reverts commit e97819ce415732b69684169ab399eb5a0ef4a96a.

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,34 @@
+const db = require("../../database/client");
+
+const insert = (user) => {
+  // eslint-disable-next-line camelcase
+  const { pseudo, email, password, registration_date, score, admin } = user;
+
+  return db.query(
+    "INSERT INTO user (pseudo, email, password, registration_date, score, admin) VALUES (?, ?, ?, ?, ?, ?)",
+    // eslint-disable-next-line camelcase
+    [pseudo, email, password, registration_date, score, admin]
+  );
+};
+
+const findById = (id) => {
+  return db.query("SELECT * FROM user WHERE id = ?", [id]);
+};
+
+const findByEmail = (pseudo, email) => {
+  return db.query("SELECT * FROM user WHERE pseudo= ? OR email = ?", [
+    pseudo,
+    email,
+  ]);
+};
+
+const findAll = () => {
+  return db.query("SELECT * FROM user");
+};
+
+module.exports = {
+  insert,
+  findById,
+  findByEmail,
+  findAll,
+};
