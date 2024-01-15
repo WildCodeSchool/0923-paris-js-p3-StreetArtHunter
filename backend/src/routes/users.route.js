d24c0eab9c479d0b1a669b325e9c0bad3b71056a
Brian-boudrioux <135231975+Brian-boudrioux@users.noreply.github.com>
2024-01-15 14:36:43 +0100

    Delete backend/src/routes/users.route.js

--- backend/src/routes/users.route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router;
-
-const userController = require("../controllers/users.controller");
-const auth = require("../middlewares/auth");
-
-router.post("/users", auth.hashPassword, userController.add);
-
-module.exports = router;
