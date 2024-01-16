32e2cfeeccebd4b0b911267d226ba824f0a16f7b
JULIEN BONET <julienbonetpro@gmail.com>
2024-01-16 20:54:38 +0100

    Finished Classement Page + random fix

--- frontend/src/pages/Classement/Classement.jsx
+++ frontend/src/pages/Classement/Classement.jsx
@@ -11,7 +11,7 @@ import OtherUserBloc from "../../components/OtherUserBloc/OtherUserBloc";
 import DataUsers from "../../../data_sample/data_users.json";
 import MostWanted from "../../assets/images/img/Most_Wanted.png";
 import SmileySearch from "../../assets/images/img/smilley.png";
-import Fissure from "../../assets/images/img/fissures3.png";
+import AnonymousKing from "../../assets/images/img/anonymous_king.png";
 import Crown from "../../assets/images/img/crown.png";
 
 function Classement() {
@@ -64,106 +64,116 @@ function Classement() {
 
   return (
     <section className="Classement_section Global_height">
-      <div className="fissure_Classement">
-        <img src={Fissure} alt="fissure" className="fissure" />
-      </div>
-
-      <div className="Classement_bloc">
-        <img src={Crown} alt="Crown" className="Crown" />
-        <h1 className="Classement_Title">hunterz Classement</h1>
-
-        <img src={MostWanted} alt="Most Wanted" className="Most_Wanted" />
-
-        <div className="searchBar_Classement_container">
-          <input
-            type="text"
-            value={search}
-            onChange={handleTyping}
-            placeholder="Who are we looking for ?"
-            className="searchBar_Classement"
-          />
+      <div className="Classement_content">
+        <div className="AnonymousKing_Classement">
           <img
-            src={SmileySearch}
-            alt="Smiley Search"
-            className="Smiley_Search_Classement"
+            src={AnonymousKing}
+            alt="Anonymous King"
+            className="AnonymousKing AnonymousKing_right"
           />
         </div>
 
-        <section className="Users_List_Classement">
-          {search === "" && (
-            <table className="UsersList_Table_Classement">
-              {currentItems.map((user, index) => (
-                <tr
-                  onClick={() => handleOpen(user)}
-                  className="UsersList_Tr_Classement"
-                  key={user.id}
-                >
-                  <td className="UsersList_Td_Classement">
-                    {(currentPage - 1) * itemsPerPage + index + 1}
-                  </td>
-                  <td className="UsersList_Td_Classement">{user.pseudo}</td>
-                  <td className="UsersList_Td_Classement">{user.score}</td>
-                  <td className="UsersList_Td_Classement">
-                    {user.registration}
-                  </td>
-                </tr>
-              ))}
-            </table>
-          )}
-          {search !== "" && (
-            <table className="UsersList_Table_Classement">
-              {filteredUsers.map((user, index) => (
-                <tr
-                  onClick={() => handleOpen(user)}
-                  className="UsersList_Tr_Classement"
-                  key={user.id}
-                >
-                  <td className="UsersList_Td_Classement">
-                    {(currentPage - 1) * itemsPerPage + index + 1}
-                  </td>
-                  <td className="UsersList_Td_Classement">{user.pseudo}</td>
-                  <td className="UsersList_Td_Classement">{user.score}</td>
-                  <td className="UsersList_Td_Classement">
-                    {user.registration}
-                  </td>
-                </tr>
-              ))}
-            </table>
-          )}
-          {/* Pagination table */}
-          <Stack spacing={2} mt={2}>
-            <Pagination
-              count={totalFilteredUsersPages}
-              size="small"
-              shape="rounded"
-              variant="outlined"
-              siblingCount={0}
-              page={currentPage}
-              onChange={handlePageChange}
-            />
-          </Stack>
-        </section>
+        <div className="Classement_bloc">
+          <img src={Crown} alt="Crown" className="Crown" />
+          <h1 className="Classement_Title">hunterz Classement</h1>
 
-        <Modal open={open} onClose={handleClose}>
-          <Box>
-            <Container maxWidth="lg">
-              <div className="modal_closed_btn_container">
-                <p onClick={handleClose} className="modal_closed_btn">
-                  X closed
-                </p>
-              </div>
-              {selectedUser && (
-                <OtherUserBloc
-                  dataUser={selectedUser}
-                  className="OtherUserModal"
-                />
-              )}
-            </Container>
-          </Box>
-        </Modal>
-      </div>
-      <div className="fissure_Classement">
-        <img src={Fissure} alt="fissure" className="fissure fissure_left" />
+          <img src={MostWanted} alt="Most Wanted" className="Most_Wanted" />
+
+          <section className="Users_List_Classement">
+            <div className="searchBar_Classement_container">
+              <input
+                type="text"
+                value={search}
+                onChange={handleTyping}
+                placeholder="Who are we looking for ?"
+                className="searchBar_Classement"
+              />
+              <img
+                src={SmileySearch}
+                alt="Smiley Search"
+                className="Smiley_Search_Classement"
+              />
+            </div>
+
+            {search === "" && (
+              <table className="UsersList_Table_Classement">
+                {currentItems.map((user, index) => (
+                  <tr
+                    onClick={() => handleOpen(user)}
+                    className="UsersList_Tr_Classement"
+                    key={user.id}
+                  >
+                    <td className="UsersList_Td_Classement">
+                      {(currentPage - 1) * itemsPerPage + index + 1}
+                    </td>
+                    <td className="UsersList_Td_Classement">{user.pseudo}</td>
+                    <td className="UsersList_Td_Classement">{user.score}</td>
+                    <td className="UsersList_Td_Classement">
+                      {user.registration}
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            )}
+            {search !== "" && (
+              <table className="UsersList_Table_Classement">
+                {filteredUsers.map((user, index) => (
+                  <tr
+                    onClick={() => handleOpen(user)}
+                    className="UsersList_Tr_Classement"
+                    key={user.id}
+                  >
+                    <td className="UsersList_Td_Classement">
+                      {(currentPage - 1) * itemsPerPage + index + 1}
+                    </td>
+                    <td className="UsersList_Td_Classement">{user.pseudo}</td>
+                    <td className="UsersList_Td_Classement">{user.score}</td>
+                    <td className="UsersList_Td_Classement">
+                      {user.registration}
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            )}
+            {/* Pagination table */}
+            <Stack spacing={2} mt={2}>
+              <Pagination
+                count={totalFilteredUsersPages}
+                size="small"
+                shape="rounded"
+                variant="outlined"
+                siblingCount={0}
+                page={currentPage}
+                onChange={handlePageChange}
+              />
+            </Stack>
+          </section>
+
+          <Modal open={open} onClose={handleClose}>
+            <Box>
+              <Container maxWidth="lg">
+                <div className="modal_closed_btn_container">
+                  <p onClick={handleClose} className="modal_closed_btn">
+                    X closed
+                  </p>
+                </div>
+                {selectedUser && (
+                  <OtherUserBloc
+                    dataUser={selectedUser}
+                    className="OtherUserModal"
+                  />
+                )}
+              </Container>
+            </Box>
+          </Modal>
+        </div>
+        <div className="AnonymousKing_Classement">
+          <img
+            src={AnonymousKing}
+            alt="Anonymous King"
+            className="AnonymousKing"
+          />
+        </div>
       </div>
     </section>
   );
