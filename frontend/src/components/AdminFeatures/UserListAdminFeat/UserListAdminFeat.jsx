5c2399465b512e5f7b8c8768560c16938cb39adf
JULIEN BONET <julienbonetpro@gmail.com>
2024-01-25 00:35:12 +0100

    fixed userlist toggle

--- frontend/src/components/AdminFeatures/UserListAdminFeat/UserListAdminFeat.jsx
+++ frontend/src/components/AdminFeatures/UserListAdminFeat/UserListAdminFeat.jsx
@@ -1,12 +1,9 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useState } from "react";
-import Pagination from "@mui/material/Pagination";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import { Container } from "@mui/material";
-import OtherUserBloc from "../../OtherUserBloc/OtherUserBloc";
+import UsersListAdBy from "../UserListAdByPseudo/UsersListAdBy";
 import DataUsers from "../../../../data_sample/data_users.json";
 import SmileySearch from "../../../assets/images/ico/smilley.png";
 import "./userListAdminFeat.css";
@@ -35,32 +32,28 @@ function UserListAdminFeat() {
       .includes(search.toLowerCase())
   );
 
-  // state pagination table //
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 10;
+  // users alphabetical sorted //
+  const userAlphabeticalSorted = data
+    .slice()
+    .sort((a, b) => a.pseudo.localeCompare(b.pseudo));
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+  // users score sorted //
+  const userScoreSorted = data.sort((a, b) => b.score - a.score);
 
-  const handlePageChange = (event, value) => {
-    setCurrentPage(value);
+  // users score sorted //
+  const userEntrySorted = data.slice().sort((a, b) => {
+    const dateA = new Date(a.registration);
+    const dateB = new Date(b.registration);
+
+    return dateA - dateB;
+  });
+
+  // Toggle Admin feature //
+  const [activeComponent, setActiveComponent] = useState("score");
+
+  const handleToggle = (id) => {
+    setActiveComponent(id);
   };
-
-  const totalFilteredUsersPages = Math.ceil(
-    filteredUsers.length / itemsPerPage
-  );
-
-  // User props Modal //
-  const [selectedUser, setSelectedUser] = useState(null);
-
-  // state modal //
-  const [open, setOpen] = useState(false);
-  const handleOpen = (userData) => {
-    setSelectedUser(userData);
-    setOpen(true);
-  };
-  const handleClose = () => setOpen(false);
 
   // >>> return <<< //
   return (
@@ -84,85 +77,51 @@ function UserListAdminFeat() {
       </div>
       <section className="ULAF_content">
         <div className="ULAF_toogle_Line">
-          <div className="ULAF_toogle" id="ULAF_by_pseudo">
+          <div
+            className={`ULAF_toogle ${
+              activeComponent === "pseudo" ? "active" : ""
+            }`}
+            onClick={() => handleToggle("pseudo")}
+            id="ULAF_by_pseudo"
+          >
             pseudo
           </div>
-          <div className="ULAF_toogle" id="ULAF_by_score">
+
+          <div
+            className={`ULAF_toogle ${
+              activeComponent === "score" ? "active" : ""
+            }`}
+            onClick={() => handleToggle("score")}
+            id="ULAF_by_score"
+          >
             score
           </div>
-          <div className="ULAF_toogle" id="ULAF_by_entry">
+
+          <div
+            className={`ULAF_toogle ${
+              activeComponent === "entry" ? "active" : ""
+            }`}
+            onClick={() => handleToggle("entry")}
+            id="ULAF_by_entry"
+          >
             entry
           </div>
         </div>
         <section className="Users_List_ULAF">
           {search === "" && (
-            <table className="UsersList_Table_ULAF">
-              {currentItems.map((user, index) => (
-                <tr
-                  onClick={() => handleOpen(user)}
-                  className="UsersList_Tr_ULAF"
-                  key={user.id}
-                >
-                  <td className="UsersList_Td_ULAF">
-                    {(currentPage - 1) * itemsPerPage + index + 1}
-                  </td>
-                  <td className="UsersList_Td_ULAF">{user.pseudo}</td>
-                  <td className="UsersList_Td_ULAF">{user.score}</td>
-                  <td className="UsersList_Td_ULAF">{user.registration}</td>
-                </tr>
-              ))}
-            </table>
-          )}
-          {search !== "" && (
-            <table className="UsersList_Table_ULAF">
-              {filteredUsers.map((user, index) => (
-                <tr
-                  onClick={() => handleOpen(user)}
-                  className="UsersList_Tr_ULAF"
-                  key={user.id}
-                >
-                  <td className="UsersList_Td_ULAF">
-                    {(currentPage - 1) * itemsPerPage + index + 1}
-                  </td>
-                  <td className="UsersList_Td_ULAF">{user.pseudo}</td>
-                  <td className="UsersList_Td_ULAF">{user.score}</td>
-                  <td className="UsersList_Td_ULAF">{user.registration}</td>
-                </tr>
-              ))}
-            </table>
-          )}
-          {/* Pagination table */}
-          <Stack spacing={2} mt={2}>
-            <Pagination
-              count={totalFilteredUsersPages}
-              size="small"
-              shape="rounded"
-              variant="outlined"
-              siblingCount={0}
-              page={currentPage}
-              onChange={handlePageChange}
+            <UsersListAdBy
+              sortedUsers={
+                activeComponent === "pseudo"
+                  ? userAlphabeticalSorted
+                  : activeComponent === "entry"
+                  ? userEntrySorted
+                  : userScoreSorted
+              }
             />
-          </Stack>
+          )}
+          {search !== "" && <UsersListAdBy sortedUsers={filteredUsers} />}
         </section>
       </section>
-
-      <Modal open={open} onClose={handleClose}>
-        <Box>
-          <Container maxWidth="lg">
-            <div className="modal_closed_btn_container">
-              <p onClick={handleClose} className="modal_closed_btn">
-                X closed
-              </p>
-            </div>
-            {selectedUser && (
-              <OtherUserBloc
-                dataUser={selectedUser}
-                className="OtherUserModal"
-              />
-            )}
-          </Container>
-        </Box>
-      </Modal>
     </section>
   );
 }
