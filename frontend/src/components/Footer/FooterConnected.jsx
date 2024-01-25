7f7f549109ed600f2d41730befd181072b795f96
JULIEN BONET <julienbonetpro@gmail.com>
2024-01-25 15:41:35 +0100

    fixed footer contact ico

--- frontend/src/components/Footer/FooterConnected.jsx
+++ frontend/src/components/Footer/FooterConnected.jsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import classement from "../../assets/images/ico/classement.png";
 import graffiti from "../../assets/images/ico/artiste-de-graffiti.png";
-import infos from "../../assets/images/ico/About_ico.png";
 import "./footer.css";
 import "./footerConnected.css";
 
@@ -62,18 +61,22 @@ function FooterConnected() {
           <img className="iconClassement" src={classement} alt="classement" />
         </div>
 
-        <div
-          role="button"
+        <svg
+          className="iconContact"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
           onClick={() => {
-            navigate("/information");
+            navigate("/contactus");
           }}
           onKeyDown={() => {
-            navigate("/information");
+            navigate("/contactus");
           }}
           tabIndex="0"
         >
-          <img className="iconInfos" src={infos} alt="infos" />
-        </div>
+          <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280L160-640v400h640v-400L480-440Zm0-80 320-200H160l320 200ZM160-640v-80 480-400Z" />
+        </svg>
       </div>
     </footer>
   );
