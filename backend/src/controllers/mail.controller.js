9f66d036e21be364f2c53ada20bcc6651b662d23
martial jouan <martialjouan@icloud.com>
2024-02-02 10:34:43 +0100

    sendmailv3

--- backend/src/controllers/mail.controller.js
+++ backend/src/controllers/mail.controller.js
@@ -9,6 +9,26 @@ const compliment = async (req, res, next) => {
   }
 };
 
+const question = async (req, res, next) => {
+  try {
+    await sendMail.sendquestion(req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const claim = async (req, res, next) => {
+  try {
+    await sendMail.sendclaim(req.body);
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   compliment,
+  question,
+  claim,
 };
--- backend/src/routes/mail.route.js
+++ backend/src/routes/mail.route.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 const mailController = require("../controllers/mail.controller");
 
 router.post("/mail/compliment", mailController.compliment);
+router.post("/mail/question", mailController.question);
+router.post("/mail/claim", mailController.claim);
 
 module.exports = router;
