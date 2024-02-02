5c6327eaab5f093c6dbe73d0a783cdbca840b1d4
JULIEN BONET <julienbonetpro@gmail.com>
2024-02-02 12:31:55 +0100

    modal change password front

--- frontend/src/pages/AdminProfil/AdminProfil.jsx
+++ frontend/src/pages/AdminProfil/AdminProfil.jsx
@@ -4,6 +4,8 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useState, useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import "./adminProfil.css";
@@ -38,6 +40,23 @@ function AdminProfil() {
   const toggleAdminInfo = () => {
     setAdminInfoIsOpen(!adminInfoIsOpen);
   };
+  // gestion modal password
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   // Toggle historical Info //
   const [adminHistoricalIsOpen, setAdminHistoricalIsOpen] = useState(false);
@@ -171,7 +190,7 @@ function AdminProfil() {
                     <span className="admin_infos_desk_font">******</span>
                   </p>
                 </div>
-                <div className="admin_infos_btn">
+                <div className="admin_infos_btn" onClick={handleOpen}>
                   <p className="text_admin_infos_btn">change password</p>
                 </div>
               </div>
@@ -349,6 +368,26 @@ function AdminProfil() {
           {activeComponent === "workValidation" && <ValidationAdminFeat />}
         </section>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <section className="password_change_container">
+            <h1 className="password_change_title">CHANGE YOUR PASSWORD</h1>
+            <input
+              type="text"
+              className="password_change_placeholder"
+              placeholder="enter new password"
+            />
+            <div className="password_change_validbtn" onClick={handleClose}>
+              VALIDATION
+            </div>
+          </section>
+        </Box>
+      </Modal>
     </section>
   );
 }
