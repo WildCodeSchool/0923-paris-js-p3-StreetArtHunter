153c24c606f639a2675929e9deb51ef5a5e20205
JULIEN BONET <julienbonetpro@gmail.com>
2024-02-03 18:07:03 +0100

    corrected level title

--- frontend/src/utils/AssignLevel.js
+++ frontend/src/utils/AssignLevel.js
@@ -1,8 +1,8 @@
 const assignLevel = (score) => {
-  if (score < 100) return "Promeneur curieux";
-  if (score >= 100 && score < 200) return "Curious Stroller";
-  if (score >= 200 && score < 500) return "Beginner of Alleys";
-  if (score >= 500 && score < 700) return "Sketch Seeker";
+  if (score < 100) return "Curious Stroller";
+  if (score >= 100 && score < 200) return "Beginner of Alleys";
+  if (score >= 200 && score < 500) return "Sketch Seeker";
+  if (score >= 500 && score < 700) return "Wall Explorer";
   if (score >= 700 && score < 900) return "Street Art Detective";
   if (score >= 900 && score < 1500) return "Graffiti Scholar";
   return "Street Art Archaeologist";
